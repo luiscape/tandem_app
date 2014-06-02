2ac9ed91d73d49f38d474d3a048a63f3a29bcca6
Luis Capelo <luiscape@gmail.com>
2014-06-01 23:12:00 -0400

    adding complete C3 export

--- c3_export.R
+++ c3_export.R
@@ -1,3 +1,9 @@
 ### Exporting for C3.js work. ### 
 
+library(rjson)
+
 InscritosDia <- data.frame(table(tandemGeral$Inscri..o))
+names(InscritosDia) <- c('Dia', 'Quantidade')
+dir.create('d3_data')
+write.csv(InscritosDia, 'd3_data/inscritos_dia.csv', row.names = FALSE)
+toJSON(InscritosDia, method = 'C')
